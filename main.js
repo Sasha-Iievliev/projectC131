6ccbc97068105996b4b677d289771d6e0a93937d
Sasha-Iievliev <121599132+Sasha-Iievliev@users.noreply.github.com>
2024-03-04 19:28:06 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+    img = loadImage("bedroom_img.jpg");
+   }
+   
+   function setup(){
+       canvas = createCanvas(640,420);
+       canvas.center();
+   }
+   
+   function draw(){
+       image(img, 0, 0, 640, 420);
+   }
